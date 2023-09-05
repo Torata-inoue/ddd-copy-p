0ea867a0d1ff451277376959777962d26fbc40ef
Torata-inoue <i_am_torao@yahoo.co.jp>
2023-09-05 20:40:54 +0900

    2.36 存在してはいけない値

--- app/first/src/ValueObject/Temporaly.php
+++ app/first/src/ValueObject/Temporaly.php
@@ -1,33 +1,3 @@
 <?php
 
-readonly class FullName
-{
-    public function __construct(
-        private FirstName $firstName,
-        private LastName $lastName
-    ) {
-
-    }
-}
-
-readonly class FirstName
-{
-    public function __construct(
-        private string $value
-    ) {
-        if (!$this->value) {
-            throw new Exception('1文字以上である必要があります。');
-        }
-    }
-}
-
-readonly class LastName
-{
-    public function __construct(
-        private string $value
-    ) {
-        if (!$this->value) {
-            throw new Exception('1文字以上である必要があります。');
-        }
-    }
-}
+$userName = 'me';
