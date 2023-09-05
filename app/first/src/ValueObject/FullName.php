231f79930505fd5828f06c30ef73ce17035b362c
Torata-inoue <i_am_torao@yahoo.co.jp>
2023-09-05 20:14:43 +0900

    2.24 FullNameでルールを担保する

--- app/first/src/ValueObject/FullName.php
+++ app/first/src/ValueObject/FullName.php
@@ -7,6 +7,18 @@ readonly class FullName
         public string $LastName,
         public string $MiddleName = ''
     ) {
+        if (!$this->FirstName) {
+            throw new Exception($this->FirstName);
+        }
+        if (!$this->LastName) {
+            throw new Exception($this->LastName);
+        }
+        if (!$this->validateName($this->FirstName)) {
+            throw new Exception('許可されていない文字が使われています');
+        }
+        if (!$this->validateName($this->LastName)) {
+            throw new Exception('許可されていない文字が使われています');
+        }
     }
 
     public function equals(FullName $other): bool
@@ -14,6 +26,12 @@ readonly class FullName
         return $this->FirstName == $other->FirstName
             && $this->LastName == $other->LastName
             && $this->MiddleName == $other->MiddleName;
+    }
+
+    private function validateName(string $value): bool
+    {
+        // アルファベットに限定
+        return true;
     }
 }
 
