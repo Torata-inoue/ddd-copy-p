c23e3788dedd5cb93b045d84e324cfb9ce1e1887
Torata-inoue <i_am_torao@yahoo.co.jp>
2023-09-05 20:25:30 +0900

    2.28 金銭の加算処理を実装する

--- app/first/src/ValueObject/Money.php
+++ app/first/src/ValueObject/Money.php
@@ -3,8 +3,16 @@
 readonly class Money
 {
     public function __construct(
-        private float $decimal,
+        private float $amount,
         private string $currency
     ) {
+    }
+
+    public function add(Money $arg): Money
+    {
+        if ($this->currency !== $arg->currency) {
+            throw new Exception('通貨が異なります');
+        }
+        return new Money($this->amount + $arg->amount, $this->currency);
     }
 }
