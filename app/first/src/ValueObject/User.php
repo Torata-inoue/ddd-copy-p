7b83f3a04c160d7858e5b29f19522adff849ec19
Torata-inoue <i_am_torao@yahoo.co.jp>
2023-09-05 20:49:11 +0900

    2.40, 41 ユーザID、ユーザ名のオブジェクト

---
+++ app/first/src/ValueObject/User.php
@@ -0,0 +1,17 @@
+<?php
+
+readonly class UserId
+{
+    public function __construct(
+        private string $value
+    ) {
+    }
+}
+
+readonly class UserName
+{
+    public function __construct(
+        private string $value
+    ) {
+    }
+}
