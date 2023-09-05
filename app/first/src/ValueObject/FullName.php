424864a214d5421a6c056d7451de537bd1002dce
Torata-inoue <i_am_torao@yahoo.co.jp>
2023-09-05 20:05:25 +0900

    2.20 新たに追加した属性の修正

--- app/first/src/ValueObject/FullName.php
+++ app/first/src/ValueObject/FullName.php
@@ -11,7 +11,9 @@ readonly class FullName
 
     public function equals(FullName $other): bool
     {
-        return $this->FirstName == $other->FirstName && $this->LastName == $other->LastName;
+        return $this->FirstName == $other->FirstName
+            && $this->LastName == $other->LastName
+            && $this->MiddleName == $other->MiddleName;
     }
 }
 
@@ -24,7 +26,7 @@ var_dump($result);
 $result2 = $nameA == $nameB;
 var_dump($result2);
 
-$badResult = $nameA->FirstName && $nameB->FirstName
-    && $nameA->LastName && $nameB->LastName
-    && $nameA->MiddleName && $nameB->MiddleName;
-var_dump($badResult);
+//$badResult = $nameA->FirstName && $nameB->FirstName
+//    && $nameA->LastName && $nameB->LastName
+//    && $nameA->MiddleName && $nameB->MiddleName;
+//var_dump($badResult);
