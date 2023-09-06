a58aa09c594ca29b388e702c11f4bcb860445759
Torata-inoue <i_am_torao@yahoo.co.jp>
2023-09-06 09:24:24 +0900

    3.2 可変なオブジェクトに変化させる

--- app/third/User.php
+++ app/third/User.php
@@ -5,8 +5,13 @@ class User
     public function __construct(
         public string $name
     ) {
+    }
+
+    public function changeName(string $name): void
+    {
         if (strlen($this->name) <= 3) {
             throw new Exception('ユーザー名は3文字以上です');
         }
+        $this->name = $name;
     }
 }
