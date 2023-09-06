ed297a433a1f5da4ce86ddd97d331aba4ece25c7
Torata-inoue <i_am_torao@yahoo.co.jp>
2023-09-06 20:48:42 +0900

    4.9 Userクラスの定義

--- app/forth/User.php
+++ app/forth/User.php
@@ -2,33 +2,12 @@
 
 namespace forth;
 
-class User
+readonly class User
 {
-    public function __construct(private readonly UserId $id, private UserName $name)
-    {
-    }
+    private UserId $userId;
 
-    // 追加した重複確認のふるまい
-    public function exists(User $user): bool
+    public function __construct(private UserName $name)
     {
-        // 重複を確認するコード
-        return true;
-    }
-
-    public function changeUserName(UserName $userName): void
-    {
-        $this->name = $userName;
+        $this->userId = new UserId();
     }
 }
-
-$checkId = new UserId("check");
-$checkName = new UseName("checker");
-$checkObj = new User($checkId, $checkName);
-
-$userId = new UserId("id");
-$userName = new UserName("nrs");
-$user = new User($userId, $userName);
-
-// 生成したオブジェクト自信に問い合わせることになる
-$result = $checkObj->exists($user);
-var_dump($result);
