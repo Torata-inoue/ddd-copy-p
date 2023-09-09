c2a5052be1759e27de2b4aca462ca371646f2be5
Torata-Inoue <i_am_torao@yahoo.co.jp>
2023-09-09 19:55:37 +0900

    5.7 リスト5.6を利用するとドメインサービスが主体とならない

--- app/fifth/UserService.php
+++ app/fifth/UserService.php
@@ -13,6 +13,9 @@ class UserService
 
     public function exists(User $user): bool
     {
+        // ユーザー名により重複確認を行うという知識は失われている
+        return $this->userRepositry->exists($user);
+
         $found = $this->userRepositry->find($user->name);
         return $found != null;
 
