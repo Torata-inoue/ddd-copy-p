9ac17b65e402929ddb056def2acf483e8f6d98b6
Torata-Inoue <i_am_torao@yahoo.co.jp>
2023-09-09 19:39:39 +0900

    5.2 UserServiceの処理内容

---
+++ app/fifth/UserService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace fifth;
+
+use PDO;
+
+class UserService
+{
+    public function exists(User $user): bool
+    {
+        $host = '127.0.0.1';  // データベースのホスト
+        $dbname = 'your_database_name';  // データベースの名前
+        $user = 'your_username';  // データベースのユーザー名
+        $pass = 'your_password';  // データベースのパスワード
+        $charset = 'utf8';  // 文字コード
+
+        $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+
+        $pdo = new PDO($dsn, $user, $pass);
+
+        $stmt = $pdo->prepare("SELECT * FROM users where id = :id");
+        $stmt->bindParam('id', $user->id);
+
+        return $stmt->fetch();
+    }
+}
