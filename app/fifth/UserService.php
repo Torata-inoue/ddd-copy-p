1cd52c0e4f9fcd4303848ca0ba53321818bdd1d2
Torata-Inoue <i_am_torao@yahoo.co.jp>
2023-09-09 20:21:14 +0900

    5.12 リポジトリをProgramクラスに渡す

--- app/fifth/UserService.php
+++ app/fifth/UserService.php
@@ -18,20 +18,5 @@ class UserService
 
         $found = $this->userRepositry->find($user->name);
         return $found != null;
-
-        $host = '127.0.0.1';  // データベースのホスト
-        $dbname = 'your_database_name';  // データベースの名前
-        $user = 'your_username';  // データベースのユーザー名
-        $pass = 'your_password';  // データベースのパスワード
-        $charset = 'utf8';  // 文字コード
-
-        $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
-
-        $pdo = new PDO($dsn, $user, $pass);
-
-        $stmt = $pdo->prepare("SELECT * FROM users where id = :id");
-        $stmt->bindParam('id', $user->id);
-
-        return $stmt->fetch();
     }
 }
