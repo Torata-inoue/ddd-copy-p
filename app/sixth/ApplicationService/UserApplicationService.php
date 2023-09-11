3668b079242f86fb2b00adc54767cb7ea9ed16ef
Torata-inoue <i_am_torao@yahoo.co.jp>
2023-09-11 09:36:31 +0900

    6.15 ユーザ名の変更を行う更新処理

--- app/sixth/ApplicationService/UserApplicationService.php
+++ app/sixth/ApplicationService/UserApplicationService.php
@@ -35,4 +35,19 @@ readonly class UserApplicationService
         $userData = new UserData($user);
         return $userData;
     }
+
+    public function update(string $userId, string $name): void
+    {
+        $targetId = new UserId($userId);
+        $user = $this->userRepository->findById($targetId);
+        if (!$user) {
+            throw new \Exception('このユーザは存在しません');
+        }
+        $newUserName = new UserName($name);
+        $user->changeName($newUserName);
+        if ($this->userService->exists($user)) {
+            throw new \Exception('ユーザは既に存在しています');
+        }
+        $this->userRepository->save($user);
+    }
 }
