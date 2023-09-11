4c56bd896ebc6ff6cd13f32d74c449172756826f
Torata-inoue <i_am_torao@yahoo.co.jp>
2023-09-11 09:29:23 +0900

    6.11 ドメインオブジェクトを引数として受け取るコンストラクタを用意する

--- app/sixth/DTO/UserData.php
+++ app/sixth/DTO/UserData.php
@@ -2,9 +2,16 @@
 
 namespace sixth\DTO;
 
+use sixth\Entity\User;
+
 class UserData
 {
-    public function __construct(public string $id, public string $name)
+    public string $id;
+    public string $name;
+
+    public function __construct(User $user)
     {
+        $this->id = $user->id->value;
+        $this->name = $user->name->value;
     }
 }
