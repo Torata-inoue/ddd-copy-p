1f5476dd5013113d7655e4ee6bcfac9ccd68e9ed
Torata-inoue <i_am_torao@yahoo.co.jp>
2023-09-12 09:32:24 +0900

    6.33 ユーザ登録処理クラス

---
+++ app/sixth/ApplicationService/UserRegisterService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace sixth\ApplicationService;
+
+use sixth\DomainService\UserService;
+use sixth\Entity\User;
+use sixth\Repository\IUserRepository;
+use sixth\ValueObject\MailAddress;
+use sixth\ValueObject\UserName;
+
+class UserRegisterService
+{
+    public function __construct(
+        private IUserRepository $userRepository,
+        private UserService $userService
+    ) {
+    }
+
+    public function handle(UserRegisterCommand $command): void
+    {
+        $user = new User(
+            new UserName($command->name),
+            new MailAddress($command->mailAddress)
+        );
+        if ($this->userService->exists($user)) {
+            throw new \Exception('ユーザは既に存在しています');
+        }
+        $this->userRepository->save($user);
+    }
+}
