dd2705a0fa6098d7aa0a453c621b1389eeff4cac
Torata-inoue <i_am_torao@yahoo.co.jp>
2023-09-12 19:57:47 +0900

    7.13 UserApplicationServiceに変化が起きた

--- app/seventh/UserApplicationService.php
+++ app/seventh/UserApplicationService.php
@@ -5,11 +5,13 @@ namespace seventh;
 class UserApplicationService
 {
     private readonly IUserRepository $userRepository;
+    private readonly IFooRepository $fooRepository;
 
     public function __construct()
     {
         // 依存解決が設定されていないのでエラーを起こす
-        $this->userRepository = ServiceLocator::resolve();
+        $this->userRepository = ServiceLocator::resolveUser();
+        $this->fooRepository = ServiceLocator::resoleveFoo();
     }
 
     public function register(UserRegisterCommand $command): void
