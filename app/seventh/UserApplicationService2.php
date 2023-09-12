cca12d403c11ea5556bc23fd6172ad1f129c4e03
Torata-inoue <i_am_torao@yahoo.co.jp>
2023-09-12 20:15:47 +0900

    7.15 新たな依存関係を追加する

--- app/seventh/UserApplicationService2.php
+++ app/seventh/UserApplicationService2.php
@@ -1,4 +1,13 @@
 <?php
 
+readonly class UserApplicationService
+{
+    public function __construct(
+        private IUserRepository $userRepository,
+        private IFooRepository $fooRepository
+    ) {
+    }
+}
+
 $userRepository = new InMemoryUserRepository();
 $userApplicationService = new UserApplicationService($userRepository);
