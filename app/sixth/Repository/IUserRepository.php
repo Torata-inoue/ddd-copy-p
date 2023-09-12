ce52beb1738f9373f63822698690f65e862156c7
Torata-inoue <i_am_torao@yahoo.co.jp>
2023-09-12 09:10:33 +0900

    6.24 リポジトリにメールアドレスによる検索手段を追加

--- app/sixth/Repository/IUserRepository.php
+++ app/sixth/Repository/IUserRepository.php
@@ -3,6 +3,7 @@
 namespace sixth\Repository;
 
 use sixth\Entity\User;
+use sixth\ValueObject\MailAddress;
 use sixth\ValueObject\UserId;
 use sixth\ValueObject\UserName;
 
@@ -10,7 +11,7 @@ interface IUserRepository
 {
     public function findById(UserId $id): ?User;
 
-    public function findByName(UserName $name): ?User;
+    public function findByMail(MailAddress $mailAddress): ?User;
 
     public function save(User $user): void;
 
---
+++ app/sixth/ValueObject/MailAddress.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace sixth\ValueObject;
+
+class MailAddress
+{
+    public function __construct(public string $value)
+    {
+    }
+}
