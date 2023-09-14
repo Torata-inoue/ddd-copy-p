8c286bbe9748f280c7437d08fe11b9acacf13ebd
Torata-inoue <i_am_torao@yahoo.co.jp>
2023-09-14 09:14:44 +0900

    9.2 採番テーブルを利用するように変更

--- app/nineth/Entity/User.php
+++ app/nineth/Entity/User.php
@@ -2,6 +2,8 @@
 
 namespace nineth\Entity;
 
+use PDO;
+
 class User
 {
     public UserId $id;
@@ -13,8 +15,20 @@ class User
 
     public function newUser(UserId $id, UserName $name): self
     {
-        $this->id = $id;
         $this->name = $name;
+
+        $host = '127.0.0.1';  // データベースのホスト
+        $dbname = 'your_database_name';  // データベースの名前
+        $user = 'your_username';  // データベースのユーザー名
+        $pass = 'your_password';  // データベースのパスワード
+        $charset = 'utf8';  // 文字コード
+
+        $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+
+        $this->pdo = new PDO($dsn, $user, $pass);
+        $stmt = $this->pdo->prepare("SELECT max('id') FROM users;");
+        $res = $stmt->fetch();
+        $this->id = $res['id'] + 1;
 
         return $this;
     }
