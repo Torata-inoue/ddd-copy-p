2cd87a7db3e7bba9997f0995b1a6998a51099c7b
Torata-inoue <i_am_torao@yahoo.co.jp>
2023-09-14 09:32:23 +0900

    9.9 オブジェクトにセッターを用意する

--- app/nineth/Entity/User.php
+++ app/nineth/Entity/User.php
@@ -9,4 +9,9 @@ class User
     public function __construct(public UserId $id, public UserName $name)
     {
     }
+
+    public function setId(UserId $id): void
+    {
+        $this->id = $id;
+    }
 }
