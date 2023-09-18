9b60bd6a006ba8df7ac731c97170000fb47d156d
Torata-Inoue <i_am_torao@yahoo.co.jp>
2023-09-18 17:48:59 +0900

    11.9 サークル参加処理のコマンドオブジェクト

--- app/eleventh/ApplicationService/CircleApplicationService.php
+++ app/eleventh/ApplicationService/CircleApplicationService.php
@@ -3,10 +3,12 @@
 namespace eleventh\ApplicationService;
 
 use eleventh\Command\CircleCreateCommand;
+use eleventh\Command\CircleJoinCommand;
 use eleventh\Domain\Entity\ICircleFactory;
 use eleventh\Domain\Repository\ICircleRepository;
 use eleventh\Domain\Repository\IUserRepository;
 use eleventh\Domain\Service\CircleService;
+use eleventh\Domain\ValueObject\CircleId;
 use eleventh\Domain\ValueObject\CircleName;
 use eleventh\Domain\ValueObject\UserId;
 use eleventh\TransactionScope;
@@ -37,6 +39,32 @@ readonly class CircleApplicationService
             throw new \Exception('サークルはすでに存在しています');
         }
 
+        $this->circleRepository->save($circle);
+
+        $transaction->complete();
+    }
+
+    public function join(CircleJoinCommand $command): void
+    {
+        $transaction = new TransactionScope();
+
+        $memberId = new UserId($command->userId);
+        $member = $this->userRepository->findById($memberId);
+        if (!$member) {
+            throw new \Exception('ユーザーが存在しませんでした');
+        }
+
+        $id = new CircleId($command->circleId);
+        $circle = $this->circleRepository->findById($id);
+        if (!$circle) {
+            throw new \Exception('サークルが見つかりませんでした');
+        }
+
+        if (count($circle->members) >= 29) {
+            throw new \Exception('サークルは30人までです');
+        }
+
+        $circle->members[] = $member;
         $this->circleRepository->save($circle);
 
         $transaction->complete();
