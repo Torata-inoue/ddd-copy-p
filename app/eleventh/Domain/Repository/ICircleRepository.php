4b5202eb7d4d28eece7f1b9fd93bacb511190600
Torata-Inoue <i_am_torao@yahoo.co.jp>
2023-09-18 17:27:29 +0900

    11.4 サークルのリポジトリ

---
+++ app/eleventh/Domain/Repository/ICircleRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace eleventh\Domain\Repository;
+
+use eleventh\Domain\Entity\Circle;
+use eleventh\Domain\ValueObject\CircleId;
+use eleventh\Domain\ValueObject\CircleName;
+
+interface ICircleRepository
+{
+    public function save(Circle $circle): void;
+
+    public function findById(CircleId $id): ?Circle;
+
+    public function findByName(CircleName $name): ?Circle;
+}
