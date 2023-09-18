8503d2709d3ea17193331c79f8fae665a70e1919
Torata-Inoue <i_am_torao@yahoo.co.jp>
2023-09-18 17:19:34 +0900

    11.2 サークル名を表す値オブジェクト

--- app/eleventh/Domain/ValueObject/CircleName.php
+++ app/eleventh/Domain/ValueObject/CircleName.php
@@ -9,6 +9,9 @@ readonly class CircleName
         if (strlen($this->value) < 3) {
             throw new \Exception('サークル名は3文字以上です');
         }
+        if (strlen($this->value) > 20) {
+            throw new \Exception('サークル名は20文字いないです');
+        }
     }
 
     public function equals(CircleName $name): bool
