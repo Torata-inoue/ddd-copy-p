7c16002ad4a2778e3f6da0c1811c2c102b39b0b9
Torata-inoue <i_am_torao@yahoo.co.jp>
2023-09-21 09:22:12 +0900

    12.10 リポジトリの永続処理

---
+++ app/twelveth/Domain/Repository/EFUserRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace twelveth\Domain\Repository;
+
+use twelveth\Domain\Entity\User;
+use twelveth\Domain\ValueObject\UserId;
+
+class EFUserRepository implements IUserRepository
+{
+
+    public function save(User $user)
+    {
+        $userDataModel = new UserDataModel($user->id, $user->name);
+        save($userDataModel);
+    }
+
+    public function findById(UserId $id): ?User
+    {
+        // TODO: Implement findById() method.
+    }
+}
