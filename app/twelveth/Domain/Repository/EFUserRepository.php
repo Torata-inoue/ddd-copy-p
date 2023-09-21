1d57ff97da58b124fc3669612281265f45fa5190
Torata-inoue <i_am_torao@yahoo.co.jp>
2023-09-21 09:32:04 +0900

    12.14 通知オブジェクトを引き渡して内部データを取得

--- app/twelveth/Domain/Repository/EFUserRepository.php
+++ app/twelveth/Domain/Repository/EFUserRepository.php
@@ -3,6 +3,7 @@
 namespace twelveth\Domain\Repository;
 
 use twelveth\Domain\Entity\User;
+use twelveth\Domain\Notification\UserDataModelBuilder;
 use twelveth\Domain\ValueObject\UserId;
 
 class EFUserRepository implements IUserRepository
@@ -10,8 +11,11 @@ class EFUserRepository implements IUserRepository
 
     public function save(User $user)
     {
-        $userDataModel = new UserDataModel($user->id, $user->name);
-        save($userDataModel);
+        $builder = new UserDataModelBuilder();
+        $user->notify($builder);
+
+        $userDataModel = $builder->build();
+        $save($userDataModel);
     }
 
     public function findById(UserId $id): ?User
